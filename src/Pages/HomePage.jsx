28253045d42ff28b133b66c6a4df823807a22881
SivaKumar202 <sivakumarjujjavarapu123@gmail.com>
2025-06-11 16:24:53 -0500

    Home Screen

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import HoodieCanvas from "../Components/Layout/HoodieCanvas";
 
-function HomePage() {
+function Home() {
   return (
-    <div>
-        <h1 className='text-3xl'>Home page</h1>
+    <div className="flex flex-col-reverse lg:flex-row items-center justify-between px-4 md:px-12 lg:px-24 py-16 gap-10">
+      {/* Text Section */}
+      <div className="w-full lg:w-1/2 text-center lg:text-left">
+        <h1 className="text-4xl md:text-5xl font-extrabold leading-tight">
+          Next-Gen Shopping Experience
+        </h1>
+        <p className="text-lg md:text-xl mt-4 text-gray-600">
+          Discover trendsetting products, seamless design, and a fully immersive
+          shopping journey crafted just for you.
+        </p>
+        <button className="mt-6 px-6 py-3 bg-blue-600 text-white font-semibold cursor-pointer rounded-xl hover:bg-blue-700 transition duration-300">
+          Shop Now
+        </button>
+      </div>
+
+      {/* 3D Model Section */}
+      <div className="w-full lg:w-1/2 h-[300px] md:h-[400px] lg:h-[500px]">
+        <Suspense fallback={<div>Loading...</div>}>
+          <HoodieCanvas />
+        </Suspense>
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default Home;
