c1490966a489649e5f0b1ad51bc820a8ce351f80
Manoj-H1180 <hostinghero5@gmail.com>
2025-06-15 21:12:18 +0530

    implemented registerpopup

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './Pages/HomePage'
-import Layout from './Components/Layout'
-import LoginPopup from './Components/LoginPopup/LoginPopup'
-import { LoginPopupProvider } from './Components/LoginPopup/LoginPopupContext'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import Layout from "./Components/Layout";
+import LoginPopup from "./Components/LoginPopup/LoginPopup";
+import { LoginPopupProvider } from "./Components/LoginPopup/LoginPopupContext";
+import RegisterPage from "./Pages/RegisterPage";
 
 function App() {
   return (
     <LoginPopupProvider>
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path='/' element={<HomePage />} />        
-      </Route>    
-    </Routes>
-    <LoginPopup/>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+      <LoginPopup />
     </LoginPopupProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/LoginPopup/LoginPopup.jsx
+++ src/Components/LoginPopup/LoginPopup.jsx
@@ -1,85 +1,88 @@
-import React,{useState, useContext, useEffect} from 'react'
-import {Link} from 'react-router-dom'
-import {Dialog, DialogTitle, DialogActions, DialogContent, Button, TextField,} from '@mui/material'
-import './LoginPopup.css'
+import React, { useState, useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import {
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  Button,
+  TextField,
+} from "@mui/material";
+import "./LoginPopup.css";
 import { FcGoogle } from "react-icons/fc";
 import { ImGithub } from "react-icons/im";
 import { SiFacebook } from "react-icons/si";
 import { AiOutlineClose } from "react-icons/ai";
-import { LoginPopupContext } from './LoginPopupContext';
+import { LoginPopupContext } from "./LoginPopupContext";
 
+function LoginPopup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const { open, handleClose } = useContext(LoginPopupContext);
 
-function LoginPopup () {
+  if (!open) return null;
 
-    const [email, setEmail] = useState ("")
-    const [password, setPassword] = useState("")
+  return (
+    <div>
+      <Dialog open={open} onClose={handleClose} className="dialog">
+        <div className="dialog-box">
+          <Button onClick={handleClose} className="dialog-close">
+            {<AiOutlineClose size={25} />}
+          </Button>
 
-    const {open, handleClose} = useContext(LoginPopupContext)
+          <DialogTitle className="dialog-title">
+            <span>Welcome Back</span>
+            <p>Please enter your Account Details</p>
+          </DialogTitle>
 
-    if(!open) return null;  
+          <DialogContent className="dialog-content">
+            <TextField
+              className="text-field"
+              autoFocus
+              label="Email"
+              id="email"
+              type="email"
+              margin="dense"
+              variant="outlined"
+              placeholder="Enter Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              fullWidth
+            />
 
-    return(
-        <div>            
-            
-            <Dialog open={open} onClose={handleClose} className='dialog'> 
+            <TextField
+              className="text-field"
+              label="Password"
+              id="password"
+              type="password"
+              margin="dense"
+              variant="outlined"
+              placeholder="Enter Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              fullWidth
+            />
 
-                <div className='dialog-box'>
-                    
-                    <Button onClick={handleClose} className='dialog-close'>{<AiOutlineClose size={25}/>}</Button>
-                    
-                           <DialogTitle className='dialog-title'>
-                                    <span>Welcome Back</span>
-                                    <p>Please enter your Account Details</p>                                                   
-                            </DialogTitle>
+            <div className="forget-password">
+              <Link>Forgot Password?</Link>
+            </div>
 
-                            <DialogContent className='dialog-content'>                              
-                               
-                                <TextField className='text-field'
-                                autoFocus                                
-                                label="Email"
-                                id = "email"
-                                type = 'email'
-                                margin = "dense"                                
-                                variant='outlined'                                
-                                placeholder='Enter Email'
-                                value = {email}
-                                onChange = {(e)=>setEmail(e.target.value)}                               
-                                fullWidth                                
-                                />
-                                
-                                <TextField className='text-field'
-                                label="Password"
-                                id = "password"
-                                type = 'password'
-                                margin = "dense"                                
-                                variant='outlined'                                
-                                placeholder='Enter Password'
-                                value = {password}
-                                onChange = {(e)=>setPassword(e.target.value)}                               
-                                fullWidth
-                                />
+            <Button className="sign-in-button">Sign in</Button>
+          </DialogContent>
 
-                                <div className='forget-password'>                                
-                                <Link>Forgot Password?</Link>
-                                </div>
-
-                                <Button  className='sign-in-button'>Sign in</Button>
-
-                            </DialogContent>
-                
-                            <DialogActions className='dialog-acions'>                               
-                                <button className='google-icon'>{<FcGoogle size={30} />}</button>
-                                <button className='github-icon'>{<ImGithub size={30}/>}</button>
-                                <button className='facebook-icon'>{<SiFacebook size={30} />}</button> 
-                            </DialogActions>
-                 </div>
-            </Dialog>
+          <DialogActions className="dialog-acions">
+            <button className="google-icon">{<FcGoogle size={30} />}</button>
+            <button className="github-icon">{<ImGithub size={30} />}</button>
+            <button className="facebook-icon">
+              {<SiFacebook size={30} />}
+            </button>
+          </DialogActions>
+          <button>Create Account</button>
         </div>
-    )
+      </Dialog>
+    </div>
+  );
 }
 
 export default LoginPopup;
-
-
-
